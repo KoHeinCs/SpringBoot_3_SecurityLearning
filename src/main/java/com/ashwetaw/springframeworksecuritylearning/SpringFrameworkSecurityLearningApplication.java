88a332dcf3b3155ca93c194ef10618764726a557
heinhtet_aung <heinhtet_aung@yomabank.com>
2024-03-01 11:52:51 +0630

    initial commit : spring boot 3 require minimum java version 17

---
+++ src/main/java/com/ashwetaw/springframeworksecuritylearning/SpringFrameworkSecurityLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ashwetaw.springframeworksecuritylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkSecurityLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrameworkSecurityLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ashwetaw/springframeworksecuritylearning/SpringFrameworkSecurityLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashwetaw.springframeworksecuritylearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkSecurityLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
